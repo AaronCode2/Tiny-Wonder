e5fd58fea6dd7e6b19e7ba686aee164793bbe516
AaronCode2 <AaronWorkPC@outlook.com>
2025-04-20 12:45:37 +0100

    Update world position and remove unnecessary directory change in App.cpp
    - Modified the world position coordinates in WorldPos.txt from {-2042, -2382} to {-1775, -1898}.
    - Removed the ChangeDirectory("build") line in App.cpp to streamline the application startup process.

--- Src/App.cpp
+++ Src/App.cpp
@@ -5,7 +5,6 @@ void App::run() {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(1000, 600, "Tiny Wonder");
     SetWindowState(FLAG_WINDOW_MAXIMIZED);
-    ChangeDirectory("build");
 
     Game game;
     game.run();
